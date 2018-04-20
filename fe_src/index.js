be25b52b5e7ad5dae90740fb268c47ecdfa92619
Christian Asmussen <christianasmussen@Christians-MBP.lbg.adapt.dk>
2018-04-20 19:06:43 +0200

    remove marker

--- fe_src/index.js
+++ fe_src/index.js
@@ -1,32 +1,33 @@
 import { TweenMax, Sine, Back } from 'gsap';
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhcHQtYXNtdXNzZW4iLCJhIjoiY2pnN3h3dmUxMzB4aDJ5bW9vb2IzNmliMSJ9.Xp1FKOwzQ9s8Q9ZEUuwZeg';
 
-let sources = [];
-const addSource = (target, key, data) => {
-  sources.push(key);
-  target.addSource(key, { type: 'geojson', data });
-  target.addLayer({
-    "id": `layer_${key}`,
-    "type": "circle",
-    "source": key,
-    "paint": {
-      "circle-radius": 18,
-      "circle-color": "#fff",
-      "circle-opacity": 0.4
-    }
-  })
-  setTimeout(removeSource, 5000, target);
-}
+// let sources = [];
+// const addSource = (target, key, data) => {
+//   sources.push(key);
+//   target.addSource(key, { type: 'geojson', data });
+//   target.addLayer({
+//     "id": `layer_${key}`,
+//     "type": "circle",
+//     "source": key,
+//     "paint": {
+//       "circle-radius": 18,
+//       "circle-color": "#fff",
+//       "circle-opacity": 0.4
+//     }
+//   })
+//   setTimeout(removeSource, 5000, target);
+// }
 
-const removeSource = (target) => {
-  const key = sources.shift();
-  target.removeLayer(`layer_${key}`);
-  target.removeSource(key);
-}
+// const removeSource = (target) => {
+//   const key = sources.shift();
+//   target.removeLayer(`layer_${key}`);
+//   target.removeSource(key);
+// }
 
-const getMarker = () => {
+const getMarker = (IconUrl) => {
   let el = document.createElement('div');
-  el.className = 'marker'
+  el.className = 'marker';
+  el.style.backgroundImage = `url(${IconUrl})`;
   return el;
 }
 
@@ -42,21 +43,25 @@ const addFeatures = (geojson, target) => {
 
 const addTempFeatures = (data, target) => {
   JSON.parse(data).forEach((item, i) => {
-    if (i > 0 ) return;
-    const markerEl = getMarker();
+    const markerEl = getMarker(item.IconUrl);
     const marker = new mapboxgl.Marker(markerEl)
       .setLngLat([item.Lat, item.Lon])
       .addTo(map);
-    TweenMax.from(markerEl, .35, {ease: Back.easeOut, alpha:0, scale:0, delay: Math.random()});
-    TweenMax.to(markerEl, .35, {ease: Sine.easeOut, alpha:0, scale:30, delay: 5});
+    const delay = Math.random()*2;
+    TweenMax.from(markerEl, .35, {ease: Back.easeOut, alpha:0, scale:0, delay: delay});
+    TweenMax.to(markerEl, .35, {ease: Back.easeIn, scale:0, delay: delay+10, onComplete:removeMarker, onCompleteParams:[marker, target]});
   })
+}
+
+const removeMarker = (marker, target) => {
+  marker.remove();
 }
 
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v9',
   center: [9.501785, 56.263920],
-  zoom: 6,
+  zoom: 7,
   minZoom: 5,
   maxZoom: 12,
 });
@@ -67,7 +72,7 @@ const setupWebSocket = () => {
 
   web_socket.onopen = (event) => {
 
-    web_socket.send("Here's some text client has sent to server upon socketd open event!");
+    web_socket.send("Here's some text client has sent to server upon socket open event!");
   };
 
   web_socket.onmessage = (event) => {
